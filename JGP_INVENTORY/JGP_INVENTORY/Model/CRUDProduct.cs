db2984fd596e038686bd7a2ad39aaa83056bd1c2
justdreaminaway <37014525+justdreaminaway@users.noreply.github.com>
2018-03-03 19:30:58 +0800

    ssdfjksdfjdsfhjsd

--- JGP_INVENTORY/JGP_INVENTORY/Model/CRUDProduct.cs
+++ JGP_INVENTORY/JGP_INVENTORY/Model/CRUDProduct.cs
@@ -8,6 +8,8 @@ using System.Data.SqlClient;
 using System.Text.RegularExpressions;
 using MySql.Data.MySqlClient;
 using System.Configuration;
+using JGP_INVENTORY.View;
+
 
 namespace JGP_INVENTORY.Model
 {
@@ -16,14 +18,14 @@ namespace JGP_INVENTORY.Model
         MySqlConnection conn = new
         MySqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
 
-        public void AddProduct(String prod_name, int prod_qty, String prod_price, int prod_id)
+        public void AddProduct(String prod_name, int prod_qty, int prod_price)
         {
-            SqlConnection con = new SqlConnection("Data Source=TESTPURU;Initial Catalog=Data;User ID=sa;Password=wintellect");
-            con.Open();
-            SqlCommand cmd = new SqlCommand("INSERT INTO product (prod_name, prod_qty, prod_price, prod_id) VALUES ('"+prod_name+"', '"+prod_qty+"', '"+prod_price+"', NULL)", con);
+            //SqlConnection con = new SqlConnection("Data Source=TESTPURU;Initial Catalog=Data;User ID=sa;Password=wintellect");
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO product (prod_name, prod_qty, prod_price, prod_id) VALUES ('"+prod_name+"', '"+prod_qty+"', '"+prod_price+"', NULL)", conn);
             cmd.CommandType = CommandType.Text;
             cmd.ExecuteNonQuery();
-            con.Close();
+            conn.Close();
         }
         public void EditProduct(String prod_name, int prod_qty, String prod_price, int prod_id)
         {
@@ -34,32 +36,24 @@ namespace JGP_INVENTORY.Model
             cmd.ExecuteNonQuery();
             con.Close();
         }
-        public void DeleteProduct(int prod_id)
+        public void DeleteProduct(String prod_name)
+
         {
-            SqlConnection con = new SqlConnection("Data Source=TESTPURU;Initial Catalog=Data;User ID=sa;Password=wintellect");
-            con.Open();
-            SqlCommand cmd = new SqlCommand("DELETE FROM product WHERE prod_id = " + prod_id, con);
+            //SqlConnection con = new SqlConnection("Data Source=TESTPURU;Initial Catalog=Data;User ID=sa;Password=wintellect");
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand("DELETE FROM product WHERE prod_name = '"+prod_name+"'", conn);
             cmd.CommandType = CommandType.Text;
             cmd.ExecuteNonQuery();
-            con.Close();
+            conn.Close();
         }
         public DataSet SearchProduct(String prod_name)
         {
-            /*
-            SqlConnection con = new SqlConnection("Data Source=TESTPURU;Initial Catalog=Data;User ID=sa;Password=wintellect");
-            con.Open();
-            SqlCommand cmd = new SqlCommand("SELECT * FROM product WHERE prod_name = "+prod_name, con);
-            cmd.CommandType = CommandType.Text;
-            cmd.ExecuteNonQuery();
-            con.Close();
-            */
-
+           // SqlConnection con = new SqlConnection("Data Source=TESTPURU;Initial Catalog=Data;User ID=sa;Password=wintellect");
             conn.Open();
-            MySqlCommand cmd = new MySqlCommand("SELECT * FROM product WHERE prod_name LIKE '%"+prod_name+"%'", conn);
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM product WHERE prod_name = "+prod_name, conn);
             MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
             DataSet ds = new DataSet();
             adp.Fill(ds, "LoadDataBinding");
-            conn.Close();
             return ds;
         }
         public DataSet DisplayProduct()
@@ -78,9 +72,116 @@ namespace JGP_INVENTORY.Model
             MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
             DataSet ds = new DataSet();
             adp.Fill(ds, "LoadDataBinding");
-            conn.Close(); 
-            
+            conn.Close();
             return ds;
         }
+        public String Register(String User, String Password, String ConfirmPass )
+        {
+            String text;
+            if (User.Length == 0)
+            {
+                text = "Enter a username.";
+                return text;
+                //textBoxEmail.Focus();
+            }
+            /* else if (!Regex.IsMatch(textBoxEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+             {
+                 errormessage.Text = "Enter a valid email.";
+                 textBoxEmail.Select(0, textBoxEmail.Text.Length);
+                 textBoxEmail.Focus();
+             }*/
+            else
+            {
+                //  string firstname = textBoxFirstName.Text;
+                //  string lastname = textBoxLastName.Text;
+                string email = User;
+                string password = Password;
+                if (Password.Length == 0)
+                {
+                    text = "Enter password.";
+                    return text;
+                   // passwordBox1.Focus();
+                }
+                else if (ConfirmPass.Length == 0)
+                {
+                    text = "Enter Confirm password.";
+                    return text;
+                  //  passwordBoxConfirm.Focus();
+                }
+                else if (Password != ConfirmPass)
+                {
+                    text = "Confirm password must be same as password.";
+                    return text;
+                   // ConfirmPass.Focus();
+                }
+                else
+                {
+                    text = "";
+                    // string address = textBoxAddress.Text;
+                    // SqlConnection con = new SqlConnection("Data Source=TESTPURU;Initial Catalog=Data;User ID=sa;Password=wintellect");
+                    conn.Open();
+
+                    // Call the function from the View Model, then the View Model will call the 
+                    // Model to execute these codes below and return the results here
+
+
+
+                    /*
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand("SELECT * FROM product", conn);
+                    MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    adp.Fill(ds, "LoadDataBinding");
+                    dataGridCustomers.DataContext = ds;
+                    */
+                    MySqlCommand cmd = new MySqlCommand("Insert into users (Username,Password) values('" + email + "','" + password + "')", conn);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    text = "You have Registered successfully.";
+                    
+                  //  Reset();
+                    return text;
+                }
+            }
+        }
+        public String Login(String User, String Pass)
+        {
+            String Text;
+            if (User.Length == 0)
+            {
+                Text = "Enter your username.";
+                //textBoxEmail.Focus();
+            }
+            else
+            {
+                string email = User;
+                string password = Pass;
+                // SqlConnection con = new SqlConnection("Data Source=TESTPURU;Initial Catalog=Data;User ID=sa;Password=wintellect");
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("Select * from users where username='" + email + "'  and password='" + password + "'", conn);
+                cmd.CommandType = CommandType.Text;
+                MySqlDataAdapter adapter = new MySqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                if (dataSet.Tables[0].Rows.Count > 0)
+                {
+                    
+                    string username = dataSet.Tables[0].Rows[0]["Username"].ToString();
+                    // page.Welcome(username);//Sending value from one form to another form.
+                    //page.Welcome.Content = username;
+                    Text = username;
+                }
+                else
+                {
+                    Text = "Sorry! Please enter existing username/password.";
+                }
+                conn.Close();
+              
+            }
+            return Text;
+        }
+     
     }
 }
--- JGP_INVENTORY/JGP_INVENTORY/ViewModel/ProductViewModel.cs
+++ JGP_INVENTORY/JGP_INVENTORY/ViewModel/ProductViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using JGP_INVENTORY.Model;
 
@@ -15,28 +14,21 @@ namespace JGP_INVENTORY.ViewModel
     {
         CRUDProduct crud = new CRUDProduct();
 
-        private static bool IsTextAllowed(string text)
-        {
-            Regex regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
-            return !regex.IsMatch(text);
-        }
 
-        public void CallAddProduct(String prod_name, int prod_qty, String prod_price, int prod_id)
+        public void CallAddProduct(String prod_name, int prod_qty, int prod_price)
         {
-            crud.AddProduct(prod_name, prod_qty, prod_price, prod_id);
+            crud.AddProduct(prod_name, prod_qty, prod_price);
         }
 
         public void CallEditProduct(String prod_name, int prod_qty, String prod_price, int prod_id)
         {
-            if (IsTextAllowed(prod_price))
-            {
-                crud.EditProduct(prod_name, prod_qty, prod_price, prod_id);
-            }
+            crud.EditProduct(prod_name, prod_qty, prod_price, prod_id);
         }
 
-        public void CallDeleteProduct(int prod_id)
+
+        public void CallDeleteProduct(String prod_name)
         {
-            crud.DeleteProduct(prod_id);
+            crud.DeleteProduct(prod_name);
         }
         public DataSet CallSearchProduct(String prod_name)
         {
@@ -45,6 +37,14 @@ namespace JGP_INVENTORY.ViewModel
         public DataSet CallDisplayProduct()
         {
             return crud.DisplayProduct();
+        }
+        public String CallRegister(String user, String Pass, String ConfirmPass)
+        {
+            return crud.Register(user, Pass, ConfirmPass);
+        }
+        public String CallLogin(String User, String Pass)
+        {
+            return crud.Login(User, Pass);
         }
     }
 }
